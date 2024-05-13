14bea2887f055fff20a4a3c3eab04914b1ee8fef
ChristianIlliano <grafica.illiano@gmail.com>
2024-05-13 17:51:46 +0200

    subdivision and comments home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -6,7 +6,49 @@ export default {
 
 <template>
     <div>
-        
+        <!-- hero carousel section 1-->
+        <header>
+            <div>
+
+            </div>
+        </header>
+
+        <!-- stylish! section 2 -->
+        <div>
+
+        </div>
+
+        <!-- mission section 3 -->
+        <div>
+
+        </div>
+
+        <!-- other project section 4 -->
+        <div>
+
+        </div>
+
+        <!-- double card section 5 -->
+        <div>
+
+        </div>
+
+        <!-- pack price section 6 -->
+        <div>
+
+        </div>
+
+        <!-- overlay citation section 7 -->
+        <div>
+
+        </div>
+
+        <!-- latast news & and out blog section 8 -->
+        <div>
+
+        </div>
+
+        <!-- preFooter section 9 -->
     </div>
 </template>
 

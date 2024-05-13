b09f5a1e7f2ca124a2758d1ea9f58d0c2dd10ee4
Robertarezza <roberta.rezza@gmail.com>
2024-05-13 20:49:28 +0200

    add: contactCards

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,15 +1,85 @@
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <template>
-    <div>
-        
+  <div class="title-contact">
+    <h1>Contact Page</h1>
+  </div>
+  <div class="container">
+    <div class="card mb-3" style="max-width: 540px">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <i class="fa-solid fa-location-dot"></i>
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </p>
+            <p class="card-text">
+              <small class="text-body-secondary">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="card mb-3" style="max-width: 540px">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <i class="fa-solid fa-location-dot"></i>
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </p>
+            <p class="card-text">
+              <small class="text-body-secondary">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card mb-3" style="max-width: 540px">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <i class="fa-solid fa-location-dot"></i>
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </p>
+            <p class="card-text">
+              <small class="text-body-secondary">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-    
+.title-contact {
+  text-align: center;
+  background-color: #f9f9f9;
+  padding: 20px;
+
+  h1 {
+    color: #00d9a6;
+    font-weight: 700;
+    font-size: 4rem;
+  }
+}
+.container{
+    display: flex;
+}
 </style>

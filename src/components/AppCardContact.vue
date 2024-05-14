d2386beb34f865faa79bd7956127af87ec156733
Robertarezza <roberta.rezza@gmail.com>
2024-05-14 11:06:06 +0200

    add: AppCardContact component

---
+++ src/components/AppCardContact.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+    data() {
+        return {
+            cardContact: [
+                {
+                    name: "ADDRESS",
+                    text: "123 Ave, Lorem City, site Country, The World",
+                    
+                },
+                {
+                    name: "PHONE",
+                    text: "(001) 123456789 - 234567891",
+                    mail: "info@phloxbusiness.com",
+                    
+                },
+                {
+                    name: "WORK HOUSE",
+                    text: "Monday - Friday       09.00 - 23.00",
+                    gg: "Sunday         09.00 - 16.00",
+                    
+                },
+            ]
+        }
+    }
+  
+};
+</script>
+
+<template>
+  <div class="row d-flex align-items-center mt-0 mr-0"v-for="index in cardContact">
+        <div class="col">
+          <i class="fa-solid fa-location-dot"></i>
+        </div>
+        <div class="col">
+          <div class="card-body" >
+            <h5 class="card-title">{{index.name}}</h5>
+            <p class="card-text">
+             {{ index.text }}
+            </p>
+            <p class="card-text">
+              {{ index.mail }} {{ index.gg }}
+            </p>
+          </div>
+        </div>
+    </div>
+        
+    
+   
+</template>
+
+<style lang="scss" scoped>
+
+.col {
+text-align: center;
+
+    .fa-solid{
+      font-size: 100px;
+      color: #00d9a6;
+    }
+
+}
+    
+
+</style>
--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,68 +1,22 @@
 <script>
-export default {};
+import AppCardContact from "../components/AppCardContact.vue";
+
+export default {
+  components: {
+    AppCardContact,
+  },
+};
 </script>
 
 <template>
   <div class="title-contact">
     <h1>Contact Page</h1>
   </div>
-  <div class="container">
-    <div class="card mb-3" style="max-width: 540px">
-      <div class="row g-0">
-        <div class="col-md-4">
-          <i class="fa-solid fa-location-dot"></i>
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This content is a little bit longer.
-            </p>
-            <p class="card-text">
-              <small class="text-body-secondary">Last updated 3 mins ago</small>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="card mb-3" style="max-width: 540px">
-      <div class="row g-0">
-        <div class="col-md-4">
-          <i class="fa-solid fa-location-dot"></i>
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This content is a little bit longer.
-            </p>
-            <p class="card-text">
-              <small class="text-body-secondary">Last updated 3 mins ago</small>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="card mb-3" style="max-width: 540px">
-      <div class="row g-0">
-        <div class="col-md-4">
-          <i class="fa-solid fa-location-dot"></i>
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This content is a little bit longer.
-            </p>
-            <p class="card-text">
-              <small class="text-body-secondary">Last updated 3 mins ago</small>
-            </p>
-          </div>
-        </div>
-      </div>
+  <div class="container ">
+    <div class="card d-flex flex-row">
+      
+          <AppCardContact />
+      
     </div>
   </div>
 </template>
@@ -79,7 +33,12 @@ export default {};
     font-size: 4rem;
   }
 }
-.container{
-    display: flex;
+.container {
+ 
+  .card{
+    border: ipx solid black;
+    
+  }
+ 
 }
 </style>

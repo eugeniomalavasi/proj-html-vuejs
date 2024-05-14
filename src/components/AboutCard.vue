5e64f7fc80cac770d8abe7ed77e9096fd2ac9c04
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-05-14 15:22:33 +0200

    feat: bootstrap js added

--- src/components/AboutCard.vue
+++ src/components/AboutCard.vue
@@ -2,24 +2,28 @@
 export default {
     props: {
         cardData: Object,
-    }
+    },
+    data() {
+        return {
+            isVisible: true,
+        }
+    },
 }
 </script>
 
 <template>
     <div class="ms-card d-flex flex-wrap">
-        <div class="front-card">
+        <div class="front-card" v-if="isVisible" @click="isVisible = !isVisible">
             <img :src="cardData.imgUrl" alt="cardData.name">
         </div>
-        <div class="back-card">
+        <div class="back-card" v-else >
             <h3 class="text-center">{{ cardData.name }}</h3>
             <h6 class="text-center">{{ cardData.role }}</h6>
             <p class="text-center">{{ cardData.text }}</p>
-
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-    
+
 </style>
--- src/components/AppCardContact.vue
+++ src/components/AppCardContact.vue
@@ -1,53 +1,48 @@
 <script>
 import { store } from '../store';
 export default {
-    data() {
-        return {
-           store,
-        }
+  data() {
+    return {
+      store,
     }
-  
+  }
+
 };
 </script>
 
 <template>
   <div class="row d-flex align-items-stretch mt-5 mr-0 pos">
-  <div class="col-md-4 mb-4 p-5" v-for="index in store.cardContact" :key="index.id">
-    <div class="card h-100 d-flex flex-column p-2">
-      <div class="card-body flex-grow-1 ">
-        <div class="row">
-          <div class="col-auto d-flex align-items-center">
-            <i :class="index.icon"></i>
-          </div>
-          <div class="col">
-            <h5 class="card-title">{{ index.name }} :</h5>
-            <p class="card-text">{{ index.text }}</p>
-            <p class="card-text">{{ index.mail }} {{ index.gg }}</p>
+    <div class="col-md-4 mb-4 p-5" v-for="index in store.cardContact" :key="index.id">
+      <div class="card h-100 d-flex flex-column p-2">
+        <div class="card-body flex-grow-1 ">
+          <div class="row">
+            <div class="col-auto d-flex align-items-center">
+              <i :class="index.icon"></i>
+            </div>
+            <div class="col">
+              <h5 class="card-title">{{ index.name }} :</h5>
+              <p class="card-text">{{ index.text }}</p>
+              <p class="card-text">{{ index.mail }} {{ index.gg }}</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
-        
-    
-   
+
+
+
 </template>
 
 <style lang="scss" scoped>
-
-.pos{
-position: relative;
-   top: -200px;
-    bottom: 0;
+.pos {
+  position: relative;
+  top: -200px;
+  bottom: 0;
 }
 
-    i{
-      font-size: 80px;
-      color: #00d9a6;
-    }
-
-
-    
-
+i {
+  font-size: 80px;
+  color: #00d9a6;
+}
 </style>

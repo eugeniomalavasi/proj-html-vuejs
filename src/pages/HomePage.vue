bb17eea64c84cd19a24c9bd43c7c261d061b86b5
ChristianIlliano <grafica.illiano@gmail.com>
2024-05-14 10:55:14 +0200

    feat particles

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,73 +5,111 @@ export default {
 </script>
 
 <template>
-    <div>
-        <!-- hero carousel section 1-->
-        <header>
-            <div>
-                <div id="carouselExample" class="carousel slide">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="..." class="d-block w-100" alt="...">
+    <main class="container">
+        <div>
+            <!-- hero carousel section 1-->
+            <header>
+                    <div id="hero-carousel" class="carousel carousel-dark slide">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0"
+                                class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
+                                aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
+                                aria-label="Slide 3"></button>
                         </div>
-                        <div class="carousel-item">
-                            <img src="..." class="d-block w-100" alt="...">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active" data-bs-interval="10000">
+                                <img src="../assets/img/redHeadWomen-on-white-chair.jpg" class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>First slide label</h5>
+                                    <p>Some representative placeholder content for the first slide.</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item " data-bs-interval="2000">
+                                <img src="../assets/img/Bussiness-employer-at-work.jpg" class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Second slide label</h5>
+                                    <p>Some representative placeholder content for the second slide.</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../assets/img/redHeadWomen-on-white-chair.jpg" class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Third slide label</h5>
+                                    <p>Some representative placeholder content for the third slide.</p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="carousel-item">
-                            <img src="..." class="d-block w-100" alt="...">
-                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#hero-carousel"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#hero-carousel"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
-                        data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
-                        data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
+            </header>
+
+            <!-- stylish! section 2 -->
+            <div class="container" id="stylish">
+                <div class="row">
+                    <div class="col-6">
+
+                    </div>
+
+                    <div class="col-6">
+
+                    </div>
                 </div>
             </div>
-        </header>
 
-        <!-- stylish! section 2 -->
-        <div>
+            <!-- mission section 3 -->
+            <div>
 
+            </div>
+
+            <!-- other project section 4 -->
+            <div>
+
+            </div>
+
+            <!-- double card section 5 -->
+            <div>
+
+            </div>
+
+            <!-- pack price section 6 -->
+            <div>
+
+            </div>
+
+            <!-- overlay citation section 7 -->
+            <div>
+
+            </div>
+
+            <!-- latast news & and out blog section 8 -->
+            <div>
+
+            </div>
+
+            <!-- preFooter section 9 -->
         </div>
-
-        <!-- mission section 3 -->
-        <div>
-
-        </div>
-
-        <!-- other project section 4 -->
-        <div>
-
-        </div>
-
-        <!-- double card section 5 -->
-        <div>
-
-        </div>
-
-        <!-- pack price section 6 -->
-        <div>
-
-        </div>
-
-        <!-- overlay citation section 7 -->
-        <div>
-
-        </div>
-
-        <!-- latast news & and out blog section 8 -->
-        <div>
-
-        </div>
-
-        <!-- preFooter section 9 -->
-    </div>
+    </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main {
+    padding-top: 100px;
+}
+
+#carouselBody {
+    width: 100%;
+    height: 100%;
+    background-color: red;
+}
+</style>

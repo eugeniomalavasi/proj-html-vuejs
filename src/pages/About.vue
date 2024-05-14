78789e9aee7700197514329fd73df737c546f7c4
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-05-14 11:07:12 +0200

    store js about part done

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,25 +1,36 @@
 <script>
 import AppButton from '../components/AppButton.vue'
+import { store } from '../store';
 export default {
     components: {
         AppButton
-    }
+    }, data() {
+        return {
+            store,
+        }
+    },
 }
 </script>
 
 <template>
-    <div class="container">
+    <!-- BUSINESS PRESENTATION PART -->
+    <section class="container">
         <div class="two-column d-flex">
             <div class="left-content">
                 <h1>We are a creative <span class="main-color">web design agency</span></h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque voluptatibus amet officiis non ipsum laborum, porro quisquam, eaque cupiditate perferendis exercitationem magnam distinctio eum? Quam, maxime asperiores? Odio, provident voluptates.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque voluptatibus amet officiis non ipsum
+                    laborum, porro quisquam, eaque cupiditate perferendis exercitationem magnam distinctio eum? Quam,
+                    maxime asperiores? Odio, provident voluptates.</p>
                 <AppButton />
             </div>
-            <img src="../assets/img/about-img/woman-hero.jpg" alt="">
+            <img src="../assets/img/about-img/woman-hero.jpg" alt="agency woman">
         </div>
-    </div>
+
+        <!-- OUR VISION PART -->
+        <h2>Our Vision</h2>
+
+
+    </section>
 </template>
 
-<style lang="scss" scoped>
-    
-</style>
+<style lang="scss" scoped></style>
---
+++ src/store.js
@@ -0,0 +1,26 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    visionArray: [
+        {
+            icon: "fa-solid fa-database",
+            title: "Virtual Documents",
+            text: "Mi chiamo Massimo Decimo Meridio, comandante dell'esercito del nord, generale delle legioni Felix, servoleale dell'unico vero imperatore Marco Aurelio"
+        },
+        {
+            icon: "fa-solid fa-sack-dollar",
+            title: "Flexible Payment",
+            text: "Mi chiamo Massimo Decimo Meridio, comandante dell'esercito del nord, generale delle legioni Felix, servoleale dell'unico vero imperatore Marco Aurelio"
+        },
+        {
+            icon: "fa-solid fa-gauge-high",
+            title: "SEO Tools",
+            text: "Mi chiamo Massimo Decimo Meridio, comandante dell'esercito del nord, generale delle legioni Felix, servoleale dell'unico vero imperatore Marco Aurelio"
+        },
+        {
+            icon: "fa-solid fa-palette",
+            title: "UI Design",
+            text: "Mi chiamo Massimo Decimo Meridio, comandante dell'esercito del nord, generale delle legioni Felix, servoleale dell'unico vero imperatore Marco Aurelio"
+        },
+    ]
+});

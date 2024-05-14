d9b107d7e3cf307cefaeefeec418d3150c67194f
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-05-14 10:06:53 +0200

    feat: test photo added

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -5,8 +5,9 @@ export default {
 </script>
 
 <template>
-    <div>
- 
+    <div class="two-column">
+        <h1>We are a creative <span class="main-color">web design agency</span></h1>
+        <img src="../assets/img/redHeadWomen-on-white-chair.jpg" alt="">
     </div>
 </template>
 

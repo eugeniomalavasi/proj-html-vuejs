ae39ca1fee975f177d2596d78a9b7a9c44651b74
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-05-14 09:22:36 +0200

    feat: logo inserted

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -22,20 +22,41 @@ export default {
 </script>
 
 <template>
-    <ul>
-        <li v-for="item in menu">
-            <router-link :to="{ name: item.routeName }" class="nav-link">{{ item.title }}</router-link>
-        </li>
-    </ul>
+
+    <section class="header-menu">
+        <!-- LOGO -->
+        <img src="../assets/img/logo.png" alt="business logo">
+        <!-- MENU LIST ITEM -->
+        <ul>
+            <li v-for="item in menu">
+                <router-link :to="{ name: item.routeName }" class="nav-link p-3">{{ item.title }}</router-link>
+            </li>
+        </ul>
+    </section>
 
 </template>
 
 <style lang="scss" scoped>
-ul {
+.header-menu {
     display: flex;
-    list-style-type: none;
-    .router-link-active {
-        color: blue;
+    height: 100px;
+    position: fixed;
+    width: 100%;
+    z-index: 2;
+
+    img {
+        width: 20%;
     }
+
+    ul {
+        display: flex;
+        list-style-type: none;
+
+
+        .router-link-active {
+            color: blue;
+        }
+    }
+
 }
 </style>

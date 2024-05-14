c89baafa19f960a26344a5fbfd638f67d217557e
Robertarezza <roberta.rezza@gmail.com>
2024-05-14 12:05:50 +0200

    fix: style AppContact

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,9 +1,13 @@
 <script>
 import AppCardContact from "../components/AppCardContact.vue";
+import AppButton from "../components/AppButton.vue";
+
+
 
 export default {
   components: {
     AppCardContact,
+    AppButton,
   },
 };
 </script>
@@ -12,13 +16,48 @@ export default {
   <div class="title-contact">
     <h1>Contact Page</h1>
   </div>
-  <div class="container ">
+  <!-- container dard -->
+  <div class="container">
     <div class="card d-flex flex-row">
-      
-          <AppCardContact />
-      
+      <AppCardContact />
     </div>
   </div>
+
+  <!-- section contact -->
+  <section>
+    <div class="cont-contact pt-5">
+        <div class="row">
+            <div class="col-md-6">
+                <h3>Please get in touch with us</h3>
+                <p>
+                    Please get in touch with us. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees.
+                </p>
+            </div>
+            <div class="col-md-6">
+                <form class="row g-3">
+                <h5>Send message</h5>
+                    <div class="col-12">
+                        <label for="Name" class="form-label">Full Name*</label>
+                        <input type="text" class="form-control" id="Name" placeholder="" />
+                    </div>
+                    <div class="col-12">
+                        <label for="exampleFormControlInput1" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="" />
+                    </div>
+                    <div class="col-12">
+                        <label for="phoneNumber" class="form-label">Please Insert Phone Number</label>
+                        <input type="tel" class="form-control" id="phoneNumber" placeholder="" />
+                    </div>
+                    <div class="col-12">
+                        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                </form>
+                <AppButton />
+            </div>
+        </div>
+    </div>
+</section>
 </template>
 
 <style lang="scss" scoped>
@@ -34,11 +73,24 @@ export default {
   }
 }
 .container {
- 
-  .card{
+  .card {
     border: ipx solid black;
-    
   }
- 
 }
+
+.section{
+  padding-top: 100px;
+}
+  h3{
+    font-size: 5rem;
+  }
+
+  h5{
+  font-size: 2.25rem;
+}
+  .form-control {
+  border: none;
+  border-bottom: 1px solid black;
+}
+
 </style>

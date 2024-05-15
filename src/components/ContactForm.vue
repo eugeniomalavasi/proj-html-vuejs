19fbfe86f4e8f47e62c1ac48cc9bce098a190b3d
Robertarezza <roberta.rezza@gmail.com>
2024-05-15 16:18:16 +0200

    fix: component contact e footer

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,118 @@
+<script>
+import AppButton from "./AppButton.vue";
+
+export default {
+components: {
+    AppButton,
+},
+}
+</script>
+
+<template>
+    <div class="col-md-5 pe-5">
+          <h3>Please get intouch withus</h3>
+          <div class="barra">
+            
+          </div>
+          <p>
+            Please get in touch with us. When, while the lovely valley teems with vapour
+            around me, and the meridian sun strikes the upper surface of the impenetrable
+            foliage of my trees, and but a few stray gleams steal into the inner
+            sanctuary. When, while the lovely valley teems with vapour around me, and the
+            meridian sun strikes the upper surface of the impenetrable foliage of my
+            trees.
+          </p>
+        </div>
+        <!-- form -->
+        <div class="col-md-7 mt-4">
+          <form class="row g-3">
+            <h5>Send message</h5>
+            <div class="col-md-4">
+              <label for="Name" class="form-label">Full Name*</label>
+              <input type="text" class="form-control" id="Name" placeholder="" />
+            </div>
+            <div class="col-md-4">
+              <label for="exampleFormControlInput1" class="form-label">Email</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder=""
+              />
+            </div>
+            <div class="col-md-4">
+              <label for="phoneNumber" class="form-label"
+                >Please Insert Phone Number</label
+              >
+              <input type="tel" class="form-control" id="phoneNumber" placeholder="" />
+            </div>
+            <div class="col-12">
+              <label for="exampleFormControlTextarea1" class="form-label"
+                >Example textarea</label
+              >
+              <textarea
+                class="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+              ></textarea>
+            </div>
+          </form>
+
+          <div class="button">
+            <AppButton />
+          </div>
+        </div>
+</template>
+
+<style lang="scss" scoped>
+@use"../style/partials/_variables" as*;
+
+.barra{
+    height: 5px;
+    background-image: linear-gradient(to right,#9be15d 0%, #00d9a6 100%);
+    content: "";
+    width: 68px;
+    border-radius: 5px;
+    margin: 0 5px;
+    margin-bottom: 10px;
+  }
+  
+  p{
+    font-size: 16px;
+    font-weight: 300;
+    line-height: 1.6em;
+  }
+
+.container {
+  .card {
+    border: ipx solid black;
+  }
+}
+
+section {
+  
+  margin: 0 2rem;
+  p{
+    font-size: 1.3rem;
+  }
+}
+h3 {
+  font-size: 3.5rem;
+  margin-bottom: 1rem;
+  font-weight: bolder;
+}
+
+h5 {
+  font-size: 2.25rem;
+}
+.form-control {
+  border: none;
+  border-bottom: 1px solid black;
+  border-radius: 0;
+}
+
+.button {
+  text-align: end;
+  margin-top: 25px;
+}
+</style>
--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,12 +1,14 @@
 <script>
 import AppCardContact from "../components/AppCardContact.vue";
 import AppButton from "../components/AppButton.vue";
+import ContactForm from "../components/ContactForm.vue";
 import { store } from '../store';
 
 export default {
   components: {
     AppCardContact,
     AppButton,
+    ContactForm,
   },
   data() {
     return {
@@ -17,79 +19,32 @@ export default {
 </script>
 
 <template>
+
+  <!-- title -->
   <div class="title-contact">
     <h1>Contact Page</h1>
   </div>
+  <!-- mappa -->
   <div class="container-fluid mt-5 ">
     <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d463149.1254326645!2d-97.165819!3d32.518685!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sit!2sit!4v1715687217406!5m2!1sit!2sit" width="1900" height="650" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-  </div>
   <!-- container card -->
-  <div class="container-fluid">
     <AppCardContact />
   </div>
 
   <!-- section contact -->
+ 
   <section >
-    <div class="cont-contact pt-5">
+    <div class="cont-contact container-fluid pt-5">
       <div class="row">
-        <div class="col-md-5 pe-5">
-          <h3>Please get in touch with us</h3>
-          <div class="barra">
-            
-          </div>
-          <p>
-            Please get in touch with us. When, while the lovely valley teems with vapour
-            around me, and the meridian sun strikes the upper surface of the impenetrable
-            foliage of my trees, and but a few stray gleams steal into the inner
-            sanctuary. When, while the lovely valley teems with vapour around me, and the
-            meridian sun strikes the upper surface of the impenetrable foliage of my
-            trees.
-          </p>
-        </div>
-        <div class="col-md-7 mt-4">
-          <form class="row g-3">
-            <h5>Send message</h5>
-            <div class="col-md-4">
-              <label for="Name" class="form-label">Full Name*</label>
-              <input type="text" class="form-control" id="Name" placeholder="" />
-            </div>
-            <div class="col-md-4">
-              <label for="exampleFormControlInput1" class="form-label">Email</label>
-              <input
-                type="email"
-                class="form-control"
-                id="exampleFormControlInput1"
-                placeholder=""
-              />
-            </div>
-            <div class="col-md-4">
-              <label for="phoneNumber" class="form-label"
-                >Please Insert Phone Number</label
-              >
-              <input type="tel" class="form-control" id="phoneNumber" placeholder="" />
-            </div>
-            <div class="col-12">
-              <label for="exampleFormControlTextarea1" class="form-label"
-                >Example textarea</label
-              >
-              <textarea
-                class="form-control"
-                id="exampleFormControlTextarea1"
-                rows="3"
-              ></textarea>
-            </div>
-          </form>
-
-          <div class="button">
-            <AppButton />
-          </div>
-        </div>
+        <ContactForm />
       </div>
     </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
+@use"../style/partials/_variables" as*;
+
 .title-contact {
   text-align: center;
   background-color: #f9f9f9;
@@ -98,56 +53,10 @@ export default {
 
  
   h1 {
-    color: #00d9a6;
+    color: $title-color;
     font-weight: 700;
     font-size: 4rem;
   }
 }
-.barra{
-    height: 5px;
-    background-image: linear-gradient(to right,#9be15d 0%, #00d9a6 100%);
-    content: "";
-    width: 68px;
-    border-radius: 5px;
-    margin: 0 5px;
-    margin-bottom: 10px;
-  }
-  p{
-    font-size: 16px;
-    font-weight: 300;
-    line-height: 1.6em;
-  }
 
-.container {
-  .card {
-    border: ipx solid black;
-  }
-}
-
-section {
-  // padding-top: 100px;
-  margin: 0 2rem;
-  p{
-    font-size: 1.3rem;
-  }
-}
-h3 {
-  font-size: 3.5rem;
-  margin-bottom: 1rem;
-  font-weight: bolder;
-}
-
-h5 {
-  font-size: 2.25rem;
-}
-.form-control {
-  border: none;
-  border-bottom: 1px solid black;
-  border-radius: 0;
-}
-
-.button {
-  text-align: end;
-  margin-top: 25px;
-}
 </style>

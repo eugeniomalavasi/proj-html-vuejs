29a20ff8ca7dd0b7fb7d32845262cb4f7f9a4711
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-05-15 11:38:47 +0200

    feat: about vision component done

--- src/components/AboutVision.vue
+++ src/components/AboutVision.vue
@@ -1,25 +1,70 @@
 <script>
 export default {
-    components: {
-
-    },
-
-    data() {
-        return {
-            
-        }
-    },
+props:  {
+    store: Object,
+}
 }
 </script>
 
 
 <template>
-    <div>
-        
+    <div class="vision-part mt-5">
+        <h2 class="text-center pt-5 pb-5 fs-1">Our Vision</h2>
+        <div class="container ms-vision">
+            <div>
+                <ul class="d-flex">
+                    <li v-for="curItem in store.visionArray" class="p-4">
+                        <div class="circle d-flex justify-content-center align-items-center">
+                            <i :class="curItem.icon" class="icon-style"></i>
+                        </div>
+                        <h5 class="text-center pt-5 h3">{{ curItem.title }}</h5>
+                        <p class="text-center fs-6 pb-5">{{ curItem.text }}</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
 
-<style lang="scsss" scoped>
-    
+<style lang="scss" scoped>
+.vision-part {
+    background-image: url(../assets/img/about-img/blur-office.jpg);
+    color: white;
+    background-size: cover;
+
+    .ms-vision {
+        height: 100%;
+        display: flex;
+        align-items: center;
+
+        li {
+            &:hover {
+                border-radius: 60px;
+                animation: fade-in 1s forwards;
+            }
+
+            @keyframes fade-in {
+                0% {
+                    background-color: rgba(0, 0, 0, 0.1);
+                }
+
+                100% {
+                    background-color: rgba(0, 0, 0, 0.3);
+                }
+            }
+        }
+
+        .circle {
+            height: 150px;
+            width: 150px;
+            border-radius: 50%;
+            border: 3px solid rgba(255, 255, 255, 0.5);
+
+            .icon-style {
+                font-size: 3rem;
+            }
+        }
+    }
+}
 </style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -3,11 +3,13 @@ import AppButton from '../components/AppButton.vue'
 import AboutCard from '../components/AboutCard.vue'
 import AboutBusiness from '../components/AboutBusiness.vue'
 import { store } from '../store';
+import AboutVision from '../components/AboutVision.vue';
 export default {
     components: {
         AppButton,
         AboutCard,
         AboutBusiness,
+        AboutVision
     }, data() {
         return {
             store,
@@ -21,22 +23,7 @@ export default {
     <AboutBusiness />
 
     <!-- OUR VISION PART -->
-    <div class="vision-part mt-5">
-        <h2 class="text-center pt-5 pb-5 fs-1">Our Vision</h2>
-        <div class="container ms-vision">
-            <div>
-                <ul class="d-flex">
-                    <li v-for="curItem in store.visionArray" class="p-4">
-                        <div class="circle d-flex justify-content-center align-items-center">
-                            <i :class="curItem.icon" class="icon-style"></i>
-                        </div>
-                        <h5 class="text-center pt-5">{{ curItem.title }}</h5>
-                        <p class="text-center fs-6 pb-5">{{ curItem.text }}</p>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
+    <AboutVision :store="store"/>
 
     <!-- OUR TEAM PART -->
     <div class="container pt-5">
@@ -59,46 +46,6 @@ export default {
 @use"../style/partials/_variables" as*;
 .two-column img {
     margin-top: 5rem;
-}
-
-.vision-part {
-    background-image: url(../assets/img/about-img/blur-office.jpg);
-    color: white;
-    background-size: cover;
-
-    .ms-vision {
-        height: 100%;
-        display: flex;
-        align-items: center;
-
-        li {
-            &:hover {
-                border-radius: 60px;
-                animation: fade-in 1s forwards;
-            }
-
-            @keyframes fade-in {
-                0% {
-                    background-color: rgba(0, 0, 0, 0.1);
-                }
-
-                100% {
-                    background-color: rgba(0, 0, 0, 0.3);
-                }
-            }
-        }
-
-        .circle {
-            height: 150px;
-            width: 150px;
-            border-radius: 50%;
-            border: 3px solid rgba(255, 255, 255, 0.5);
-
-            .icon-style {
-                font-size: 3rem;
-            }
-        }
-    }
 }
 
 .ms-contact-us {
